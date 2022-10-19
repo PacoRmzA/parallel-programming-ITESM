881b6fa65a76aa720eae7649a085e32c9f3cfa32
PacoRmzA <a01634262@tec.mx>
2022-10-18 23:06:30 -0500

    Matrix mp fixes

--- matrix_mp/matrix_mp.c
+++ matrix_mp/matrix_mp.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** matrix_mp(int m, int p, int (*mat1)[m], int (*mat2)[p]) {
-    int n,q;
-    n = sizeof(mat1[0])/sizeof(mat1[0][0]);
-    q = sizeof(mat2[0])/sizeof(mat2[0][0]);
-
+int** matrix_mp(int m, int n, int p, int q, int mat1[m][n], int mat2[p][q]) {
     if (n != p) return 0;
 
     int** result;
     result = malloc(sizeof(int*) * m);
-    for (int i=0; i<m; i++) result[i] = malloc(sizeof(int) * q);
+    for (int i=0; i<m; i++) result[i] = malloc(sizeof(int*) * q);
     
     for (int i=0; i<m; i++) {
         for (int j=0; j<q; j++) {
@@ -26,12 +22,21 @@ int** matrix_mp(int m, int p, int (*mat1)[m], int (*mat2)[p]) {
 int main() {
     int mat1[2][3] = {{1,2,3},{4,5,6}};
     int mat2[3][2] = {{7,8},{9,10},{11,12}};
-    int** mat3= matrix_mp(sizeof(mat1)/sizeof(mat1[0]), sizeof(mat1)/sizeof(mat1[0]), mat1, mat2);
+    int m, n, p, q;
+    m = sizeof(mat1)/sizeof(mat1[0]);
+    n = sizeof(mat1[0])/sizeof(mat1[0][0]);
+    p = sizeof(mat2)/sizeof(mat2[0]);
+    q = sizeof(mat2[0])/sizeof(mat2[0][0]);
+    int** mat3 = matrix_mp(m, n, p, q, mat1, mat2);
     for (int i=0; i<2; i++) {
         for (int j=0; j<2; j++) {
             printf("%d ",mat3[i][j]);
         }
         printf("\n");
     }
+    for (int i=0; i<2; i++) {
+        free(mat3[i]);
+    }
+    free(mat3);
     return 0;
 }
